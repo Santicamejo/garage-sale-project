b23b7461a4d8964ddeef0de84a22e4ff933f813c
Santiago Miranda <santiagocamejo21@gmail.com>
2024-06-13 18:09:29 -0300

    Agrego los menues de mobile

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hambMenu = document.querySelector('.mobile-menu')
+const menuHambLogo = document.querySelector('.menu')
+
 
 menuEmail.addEventListener('mouseover', toggleDesktopMenu);
+menuHambLogo.addEventListener('click', togglehambMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
   console.log("Menu de Email Activo!")
 }
+
+function togglehambMenu() {
+  hambMenu.classList.toggle('inactive');
+  console.log("Menu de hamburguesa Activo!");
+}
+
 
 
 
