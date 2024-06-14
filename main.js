cb74726f6aa265403e9073198e1ab554c2c27474
Santiago Miranda <santiagocamejo21@gmail.com>
2024-06-14 19:21:37 -0300

    Agrege la funcion de carrito

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const hambMenu = document.querySelector('.mobile-menu')
 const menuHambLogo = document.querySelector('.menu')
+const aside = document.querySelector('.product-detail')
 
 
-menuEmail.addEventListener('mouseover', toggleDesktopMenu);
+menuEmail.addEventListener('click ', toggleDesktopMenu);
 menuHambLogo.addEventListener('click', togglehambMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
@@ -15,6 +18,17 @@ function toggleDesktopMenu() {
 function togglehambMenu() {
   hambMenu.classList.toggle('inactive');
   console.log("Menu de hamburguesa Activo!");
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuOpen = !hambMenu.classList.contains('inactive');
+  const isAside = !aside.classList.contains('inactive');
+
+
+  aside.classList.toggle('inactive');
+  if (hambMenu.classList.contains('inactive')) {
+
+  }
 }
 
 
