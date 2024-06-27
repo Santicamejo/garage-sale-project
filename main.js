10db205a977a45b0fcb38f14d86d1fae51da12ee
Santiago Miranda <santiagocamejo21@gmail.com>
2024-06-27 19:15:51 -0300

    Trabaje en la logica de los menués para que no se solapen y repare el menu desktop

--- main.js
+++ main.js
@@ -6,29 +6,27 @@ const menuHambLogo = document.querySelector('.menu')
 const aside = document.querySelector('.product-detail')
 
 
-menuEmail.addEventListener('click ', toggleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHambLogo.addEventListener('click', togglehambMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle('inactive');
-  console.log("Menu de Email Activo!")
+  aside.classList.add('inactive');
+  console.log("Menu de Email Activo!");
 }
 
 function togglehambMenu() {
   hambMenu.classList.toggle('inactive');
+  aside.classList.add('inactive');
+
   console.log("Menu de hamburguesa Activo!");
 }
 
 function toggleCarritoAside() {
-  const isMobileMenuOpen = !hambMenu.classList.contains('inactive');
-  const isAside = !aside.classList.contains('inactive');
-
-
-  aside.classList.toggle('inactive');
-  if (hambMenu.classList.contains('inactive')) {
-
-  }
+  aside.classList.toggle('inactive'); //*con el toggel lo que logramos es que la clase .inactive se desaparezca del aside y asi mostrarlo//*
+  hambMenu.classList.add('inactive'); //*con el add lo que logramos es que la clase inactive se agrege al hambMenu y asi desaparezca cuando se abre el aside(CarritoDeCompras)//*
+  desktopMenu.classList.add('inactive'); //*con el add lo que logramos es que la clase inactive se agrege al desktopMenu y asi desaparezca cuando se abre el aside(CarritoDeCompras)//*
 }
 
 
