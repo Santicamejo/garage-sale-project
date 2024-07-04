3f38c4b064efa06d6189cca12c4e0a4175935793
Santiago Miranda <santiagocamejo21@gmail.com>
2024-07-04 11:31:20 -0300

    Logica de apertura de ventanas terminada

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
-const menuEmail= document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
-const hambMenu = document.querySelector('.mobile-menu')
-const menuHambLogo = document.querySelector('.menu')
-const aside = document.querySelector('.product-detail')
-const cardsContainer =  document.querySelector('.cards-container')
+const emailMenu= document.querySelector('.navbar-email');
+const userMenu = document.querySelector('.desktop-menu');
+const menuCartICON = document.querySelector('.navbar-shopping-cart')
+const hamburgerMenu = document.querySelector('.mobile-menu')
+const hamburgerMenuICON = document.querySelector('.menu')
+const carritoAside = document.querySelector('.product-detail')
+const productcarritoAsideBOX =  document.querySelector('.cards-container')
+const productCarritoAside = document.querySelector('.product-detail-bigcard');
+const cruzProductCarritoAside = document.querySelector('.product-detail-close-bigcard');
 
 
+emailMenu.addEventListener('click', toggleuserMenu);
+hamburgerMenuICON.addEventListener('click', togglehamburgerMenu);
+menuCartICON.addEventListener('click', toggleCarritocarritoAside);
+cruzProductCarritoAside.addEventListener('click', cerrarProductcarritoAside);
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHambLogo.addEventListener('click', togglehambMenu);
-menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
-  aside.classList.add('inactive');
-  console.log("Menu de Email Activo!");
+function toggleuserMenu() {
+  userMenu.classList.toggle('inactive');
+  carritoAside.classList.add('inactive');
+  productCarritoAside.classList.add('inactive');
 }
 
-function togglehambMenu() {
-  hambMenu.classList.toggle('inactive');
-  aside.classList.add('inactive');
-
-  console.log("Menu de hamburguesa Activo!");
+function togglehamburgerMenu() {
+  hamburgerMenu.classList.toggle('inactive');
+  carritoAside.classList.add('inactive');
+  productCarritoAside.classList.add('inactive');
 }
 
-function toggleCarritoAside() {
-  aside.classList.toggle('inactive'); //*con el toggel lo que logramos es que la clase .inactive se desaparezca del aside y asi mostrarlo//*
-  hambMenu.classList.add('inactive'); //*con el add lo que logramos es que la clase inactive se agrege al hambMenu y asi desaparezca cuando se abre el aside(CarritoDeCompras)//*
-  desktopMenu.classList.add('inactive'); //*con el add lo que logramos es que la clase inactive se agrege al desktopMenu y asi desaparezca cuando se abre el aside(CarritoDeCompras)//*
+function toggleCarritocarritoAside() {
+  carritoAside.classList.toggle('inactive'); //*con el toggel lo que logramos es que la clase .inactive se desaparezca del carritoAside y asi mostrarlo//*
+  hamburgerMenu.classList.add('inactive'); //*con el add lo que logramos es que la clase inactive se agrege al hamburgerMenu y asi desaparezca cuando se abre el carritoAside(CarritoDeCompras)//*
+  userMenu.classList.add('inactive'); //*con el add lo que logramos es que la clase inactive se agrege al userMenu y asi desaparezca cuando se abre el carritoAside(CarritoDeCompras)//*
+  productCarritoAside.classList.add('inactive');//*con el add lo que logramos es que la clase inactive se agrege al productCarritoAside y asi desaparezca cuando se abre el carritoAside(CarritoDeCompras)//*
 }
+
+function openProductcarritoAside(){
+  productCarritoAside.classList.remove('inactive')
+  hamburgerMenu.classList.add('inactive');
+  userMenu.classList.add('inactive');
+  carritoAside.classList.add('inactive');
+}
+
+function cerrarProductcarritoAside(){
+  productCarritoAside.classList.add('inactive')
+}
+
 
 const productList = [];
 productList.push({
@@ -49,17 +64,17 @@ productList.push({
 });
 productList.push({
   name: 'Car',
-  price: '12.000',
+  price: '32.000',
   image: './fotos/car.png',
 });
 productList.push({
   name: 'Phone',
-  price: '2100',
+  price: '1200',
   image: './fotos/phone.png',
 });
 productList.push({
   name: 'Bottle',
-  price: '60',
+  price: '3',
   image: './fotos/bottle.png',
 });
 productList.push({
@@ -69,6 +84,7 @@ productList.push({
 });
 // creamos los productos y les colocamos los datos, luego hacemos un push al array
 
+
 function renderProducts(arr){
   for (product of arr){
     const productCard = document.createElement('div');
@@ -76,6 +92,7 @@ function renderProducts(arr){
   
     const productImg =  document.createElement('img');
     productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductcarritoAside); 
   
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -103,8 +120,10 @@ function renderProducts(arr){
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
   
-    cardsContainer.appendChild(productCard);
+    productcarritoAsideBOX.appendChild(productCard);
   }
 }
-// por orden creamos una funcion para poder mostrar cualquier array en el formato de carrito con el for y de parametro le pasamos arr que es el array que desemos
+
+
+// por orden creamos una funcion para poder mostrar cualquier array en el formato de Carrito con el for y de parametro le pasamos arr que es el array que desemos
 renderProducts(productList);
